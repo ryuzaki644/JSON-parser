1003d54c06a7a367204aed32b17821004a2d2663
ryuzaki644 <sathwikkoushal@gmail.com>
2017-07-27 15:20:26 +0530

    Add parser factory , array and object parsers  -Add example.json

--- JSONparser.js
+++ JSONparser.js
@@ -47,3 +47,78 @@ var colonParser = function (input) {
   }
   return null
 }
+
+var arrayParser = function (input) {
+  if (input.startsWith('[') === false) return null
+  input = input.slice(1)
+  var array = []
+  var result, comma
+  while (true) {
+    input = input.trim()
+    result = valueParser(input)
+    // console.log(result)
+    if (result === null) return null
+    array.push(result[0])
+    input = result[1].trim()
+    comma = commaParser(input)
+    if (comma === null) break
+    input = comma[1]
+  }
+  input = input.trim()
+  if (input.startsWith(']') === true) {
+    return [array, input.slice(1)]
+  }
+  return null
+}
+
+var objectParser = function (input) {
+  input = input.trim()
+  var coma, res, key, value
+  if (input.startsWith('{') === false) return null
+  input = input.slice(1)
+  var object = {}
+  while (input) {
+    res = stringParser(input.trim())
+    if (res === null) break
+    key = res[0]
+    value = (res[1]).trim()
+    value = colonParser(value)
+    if (value === null) {
+      return null
+    }
+    value = (value[1]).trim()
+    value = valueParser(value)
+    if (value === null) return null
+    object[key] = value[0]
+    input = value[1]
+    coma = commaParser(input)
+    if (coma === null) break
+    input = coma[1]
+  }
+  if (input.startsWith('}') === true) {
+    return [object, input.slice(1)]
+  }
+  return null
+}
+
+var anyOneParserFactory = function (...parsers) {
+  return function (input) {
+    return parsers.reduce(function (accum, parser) {
+      if (accum === null) {
+        return parser(input)
+      } else return accum
+    },
+    null)
+  }
+}
+
+var valueParser = anyOneParserFactory(nullParser, booleanParser, numberParser, stringParser, arrayParser, objectParser)
+
+var inp = require('fs').readFileSync('./example.json').toString()
+var out = valueParser(inp)
+
+if (out === null) {
+  console.log('INVALID JSON')
+} else {
+  console.log(JSON.stringify(out[0], null, 2))
+}
